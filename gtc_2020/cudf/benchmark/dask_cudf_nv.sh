aa0b280a209690f65dbe5a52a212f6c53707a888
Zahra Ronaghi <zronaghi@gmail.com>
2020-09-08 12:09:41 -0400

    add rapids path

--- gtc_2020/cudf/benchmark/dask_cudf_nv.sh
+++ gtc_2020/cudf/benchmark/dask_cudf_nv.sh
@@ -1,13 +1,19 @@
-NUM_WORKERS=6
+NUM_WORKERS=4
 PACKAGE="dask_cudf"
 SCRIPT="cudf_benchmarking_nv.py"
 PROJ_ID="gen119"
 SCHEDULER_JSON="$MEMBERWORK/$PROJ_ID/dask/my-scheduler-gpu.json"
 
+module load gcc/7.4.0
+module load cuda/10.1.243
+module load python/3.7.0-anaconda3-5.3.0
+
+export PATH=$WORLDWORK/stf011/nvrapids_0.14_gcc_7.4.0/bin:$PATH
+
 # I/O - TESTING CHUNKING
 
 #python $SCRIPT --package $PACKAGE --num_dask_workers $NUM_WORKERS --scheduler_json_path $SCHEDULER_JSON --file_size 20G --read_chunk_mb 4096 --stop_at_read True
-python $SCRIPT --package $PACKAGE --num_dask_workers $NUM_WORKERS --scheduler_json_path $SCHEDULER_JSON --file_size 25G --stop_at_read True
+#python $SCRIPT --package $PACKAGE --num_dask_workers $NUM_WORKERS --scheduler_json_path $SCHEDULER_JSON --file_size 25G --stop_at_read True
 
 # Partitioning
 
