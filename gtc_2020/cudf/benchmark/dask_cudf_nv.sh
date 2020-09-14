2fc103e93eef09a815d4322c4ffaa2569c55b994
Zahra Ronaghi <zronaghi@gmail.com>
2020-09-14 09:10:55 -0400

    update cudf script

--- gtc_2020/cudf/benchmark/dask_cudf_nv.sh
+++ gtc_2020/cudf/benchmark/dask_cudf_nv.sh
@@ -1,15 +1,24 @@
-NUM_WORKERS=6
-PACKAGE="dask_cudf"
+NUM_WORKERS=42
+PACKAGE="dask"
+#PACKAGE="dask_cudf"
+#PACKAGE="cudf"
 SCRIPT="cudf_benchmarking_nv.py"
 PROJ_ID="gen119"
-SCHEDULER_JSON="$MEMBERWORK/$PROJ_ID/dask/my-scheduler-gpu.json"
+SCHEDULER_JSON="$MEMBERWORK/$PROJ_ID/dask/my-scheduler.json"
+#SCHEDULER_JSON="$MEMBERWORK/$PROJ_ID/dask/my-scheduler-gpu.json"
+
+module load gcc/7.4.0
+module load cuda/10.1.243
+module load python/3.7.0-anaconda3-5.3.0
+
+export PATH=$WORLDWORK/stf011/nvrapids_0.14_gcc_7.4.0/bin:$PATH
 
 # I/O - TESTING CHUNKING
 
 #python $SCRIPT --package $PACKAGE --num_dask_workers $NUM_WORKERS --scheduler_json_path $SCHEDULER_JSON --file_size 20G --read_chunk_mb 4096 --stop_at_read True
-python $SCRIPT --package $PACKAGE --num_dask_workers $NUM_WORKERS --scheduler_json_path $SCHEDULER_JSON --file_size 25G --stop_at_read True
+#python $SCRIPT --package $PACKAGE --num_dask_workers $NUM_WORKERS --scheduler_json_path $SCHEDULER_JSON --file_size 10G --stop_at_read True
 
 # Partitioning
 
-python $SCRIPT --package $PACKAGE --num_dask_workers $NUM_WORKERS --scheduler_json_path $SCHEDULER_JSON --file_size 25G
-
+#python $SCRIPT --package $PACKAGE --num_dask_workers $NUM_WORKERS --scheduler_json_path $SCHEDULER_JSON --file_size 25G
+python $SCRIPT --package $PACKAGE --num_dask_workers $NUM_WORKERS --scheduler_json_path $SCHEDULER_JSON --file_size 25G --num_partitions 96
